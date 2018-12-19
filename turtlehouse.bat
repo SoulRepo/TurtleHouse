c5b0cf5a093f3d9a1ab12d0523c9b467f9501189
SoulRepo <aedencloud@icloud.com>
2018-12-18 19:57:26 -0500

    Reupdate

--- turtlehouse.bat
+++ turtlehouse.bat
@@ -1,54 +1,74 @@
 @echo off
-if not exist "%appdata%\TurtleHouse" rm "%appdata%\TurtleHouse"
-if not exist "%username\Desktop\Debug.txt" goto nodebug
-title TurtleHouse Secret Debug
-set debug=1
-echo                 _______         _   _      _    _                      
-echo                |__   __|       | | | |    | |  | |                     
-echo                   | |_   _ _ __| |_| | ___| |__| | ___  _   _ ___  ___  
-echo                   | | | | | '__| __| |/ _ \  __  |/ _ \| | | / __|/ _ \      
-echo                   | | |_| | |  | |_| |  __/ |  | | (_) | |_| \__ \  __/
-echo                   |_|\__,_|_|   \__|_|\___|_|  |_|\___/ \__,_|___/\___|
-echo       
-echo                     Welcome to the secret debug menu of TurtleHouse!
-echo                                    Choose an option!
-echo                 NOTE: Debug can only be exited by exiting by the program.
+set test=1
+set version=Version 0.1
+if not exist "%appdata%\Roaming\TurtleHouse" mkdir "%appdata%\Roaming\TurtleHouse"
+if not exist "%username\Desktop\Debug.txt" goto setup
+:setup
+set FilesHostedOn=https://raw.githubusercontent.com/KcrPL/KcrPL.github.io/master/Patchers_Auto_Update/WiiWare-Patcher
+title TurtleHouse Sound Test %version%
+set test=1
+echo.
+echo.
+echo.
+echo        ######## ##     ## ########  ######## ##       ######## ##     ##  #######  ##     ##  ######  ######## 
+echo           ##    ##     ## ##     ##    ##    ##       ##       ##     ## ##     ## ##     ## ##    ## ##     
+echo           ##    ##     ## ##     ##    ##    ##       ##       ##     ## ##     ## ##     ## ##       ##       
+echo           ##    ##     ## ########     ##    ##       ######   ######### ##     ## ##     ##  ######  ######   
+echo           ##    ##     ## ##   ##      ##    ##       ##       ##     ## ##     ## ##     ##       ## ##       
+echo           ##    ##     ## ##    ##     ##    ##       ##       ##     ## ##     ## ##     ## ##    ## ##    
+echo           ##     #######  ##     ##    ##    ######## ######## ##     ##  #######   #######   ######  ########    
+echo.
+echo.
+echo.
+echo                                              Welcome to TurtleHouse Debug!
+echo                                                    Choose an option!
+echo.                            
+echo                                                 1: Reset House Directory
+echo                                                  2: Reset Announcements
+echo                                                  3: Connect to Online
+echo                                                        4: About
+echo                                                   5: Exit to desktop
+echo.
+set /p Input=Choose the option: 
 :nodebug
-set debug=0
-title TurtleHouse Launcher
-echo                 _______         _   _      _    _                      
-echo                |__   __|       | | | |    | |  | |                     
-echo                   | |_   _ _ __| |_| | ___| |__| | ___  _   _ ___  ___  
-echo                   | | | | | '__| __| |/ _ \  __  |/ _ \| | | / __|/ _ \      
-echo                   | | |_| | |  | |_| |  __/ |  | | (_) | |_| \__ \  __/
-echo                   |_|\__,_|_|   \__|_|\___|_|  |_|\___/ \__,_|___/\___|
-echo   
-echo      
-echo                                  Welcome to TurtleHouse!
-echo                                     Choose an option!
-echo                                 
-echo                                       1: Load House
-echo                                      2: Switch House
-echo                                    3: Connect to Online
-echo                                          4: About
-echo                                          5: Leave
-CHOICE /C 12345 /M "Choose an option:"
+set test=0
+title TurtleHouse Launcher %version%
+echo.
+echo.
+echo.
+echo        ######## ##     ## ########  ######## ##       ######## ##     ##  #######  ##     ##  ######  ######## 
+echo           ##    ##     ## ##     ##    ##    ##       ##       ##     ## ##     ## ##     ## ##    ## ##     
+echo           ##    ##     ## ##     ##    ##    ##       ##       ##     ## ##     ## ##     ## ##       ##       
+echo           ##    ##     ## ########     ##    ##       ######   ######### ##     ## ##     ##  ######  ######   
+echo           ##    ##     ## ##   ##      ##    ##       ##       ##     ## ##     ## ##     ##       ## ##       
+echo           ##    ##     ## ##    ##     ##    ##       ##       ##     ## ##     ## ##     ## ##    ## ##    
+echo           ##     #######  ##     ##    ##    ######## ######## ##     ##  #######   #######   ######  ########    
+echo.
+echo.
+echo.
+echo                                                 Welcome to TurtleHouse!
+echo                                                    Choose an option!
+echo.                            
+echo                                                      1: Load House
+echo                                                     2: Switch House
+echo                                                  3: Connect to Online
+echo                                                        4: About
+echo                                                   5: Exit to desktop
+echo.
+set /p Input=Choose the option: 
 
-:: Settings
-IF ERRORLEVEL 5 GOTO Leave
-IF ERRORLEVEL 4 GOTO Logoff
-IF ERRORLEVEL 3 GOTO CloseAllWindows
-IF ERRORLEVEL 2 GOTO Shutdown
-IF ERRORLEVEL 1 GOTO Restart
+if %input% 1 goto loadhouse
+if %input% 2 goto switchhouse
 
-:LoadHouse
+:loadhouse
 ECHO Loading the house...
-open 
+open "housefiles\%housename%.py"
+echo House loaded! If it did not load, check the house name.
 
 
-:Shutdown
-ECHO Shutdown (put your shutdown code here)
-GOTO End
+:switchhouse
+echo Please insert the house name.
+echo 
 
 :CloseAllWindows
 ECHO Close All Windows (put your close all windows code here)
@@ -62,9 +82,8 @@ GOTO End
 ECHO Switch User (put your switch user code here)
 GOTO End
 
-:Leave
+:leave
 title Exiting program...
 echo Bye!
-set debug=0
-if exist "%username%\Desktop\Debug.txt" del "%username%\Desktop\Debug.txt"
+set test=0
 exit
